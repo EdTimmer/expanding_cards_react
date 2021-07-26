6dc8a9ba7b0e2f8aff6011dd4b703f270b887bd9
Ed Timmer <edtimmer@gmail.com>
2021-07-26 09:41:46 -0400

    added grid solution

--- src/flex_components/App.jsx
+++ src/flex_components/App.jsx
@@ -31,7 +31,7 @@ const App = () => {
       title: 'Mountains - Clouds'
     }
   ];
-
+  
   return (
     <div className="container">
       {
---
+++ src/grid_components/App.grid.jsx
@@ -0,0 +1,51 @@
+import { useState } from 'react';
+
+import './App.grid.css';
+import Panel from './Panel.grid';
+
+const App = () => {
+  const [activeIndex, setActiveIndex] = useState(0)
+  
+  const handleClick = (index) => {
+    setActiveIndex(index)
+  }
+  const panels = [
+    {
+      img: 'https://images.unsplash.com/photo-1558979158-65a1eaa08691?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80', 
+      title: 'Explore The World'
+    },
+    {
+      img: 'https://images.unsplash.com/photo-1572276596237-5db2c3e16c5d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80', 
+      title: 'Wild Forest'
+    },
+    {
+      img: 'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1353&q=80', 
+      title: 'Sunny Beach'
+    },
+    {
+      img: 'https://images.unsplash.com/photo-1551009175-8a68da93d5f9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1351&q=80', 
+      title: 'City on Winter'
+    },
+    {
+      img: 'https://images.unsplash.com/photo-1549880338-65ddcdfd017b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80', 
+      title: 'Mountains - Clouds'
+    }
+  ];
+  
+  const clName = `container active-${activeIndex}` 
+  
+  return (
+    <div className={clName}>
+      {
+        panels.map((panel, index) => {
+          const panelClName = `panel ${activeIndex === index ? 'active' : ''}` 
+          return (
+            <Panel background={panel.img} title={panel.title} key={index} className={panelClName} handleClick={handleClick} index={index}/>
+          )
+        })
+      }
+    </div>
+  )
+}
+
+export default App;
